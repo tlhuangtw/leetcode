c9f3100a02951f5ab3ee6b21b5b2fb1dc44b8b54
Tian-Liang Huang <tl@rexlite.com.tw>
2026-04-09 14:07:34 +0800

    Created 868.py

---
+++ 868.py
@@ -0,0 +1,17 @@
+# ref: https://leetcode.com/problems/binary-gap/solutions/7597944/binary-gap-by-la_castille-22cn
+
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        ans, cur = 0, 0
+        n //= n & -n  # remove trailing zero bits
+        if n == 1:  # there is only one '1'
+            return 0
+        while n > 0:
+            if n % 2 == 0:
+                cur += 1
+            else:
+                ans = max(cur, ans)
+                cur = 0
+            n //= 2
+        return ans + 1
+
