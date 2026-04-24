fe9a80e0eaa202f2d7aed99639cd234fd9146cae
Tian-Liang Huang <tl@rexlite.com.tw>
2026-04-24 11:13:33 +0800

    Created 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int expandAroundCenter(const string& s, int left, int right) {
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+
+    string longestPalindrome(string s) {
+        int start = 0, maxLength = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+
+            int len = max(len1, len2);
+            if (len > maxLength) {
+                maxLength = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+
+        return s.substr(start, maxLength);
+    }
+};
+
