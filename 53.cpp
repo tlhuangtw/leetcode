4d2254cee2efa1ecebb7613a9a9007612f5837ac
Tian-Liang Huang <tianliang.huang@welhunt.com>
2026-05-07 17:46:44 +0800

    Created 53.cpp

---
+++ 53.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxEndingHere = nums[0], maxSoFar = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            maxEndingHere = max(maxEndingHere + nums[i], nums[i]);
+            maxSoFar = max(maxSoFar, maxEndingHere);
+        }
+
+        return maxSoFar;
+    }
+};
+
