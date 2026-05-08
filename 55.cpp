e444ffcdd480073f0e0aad46879b4116f361d8a6
Tian-Liang Huang <tianliang.huang@welhunt.com>
2026-05-08 11:38:47 +0800

    Created 55.cpp

---
+++ 55.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0, n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            if (maxReach < i) {
+                return false;
+            }
+            if (maxReach >= n - 1) {
+                return true;
+            }
+            maxReach = max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+};
+
