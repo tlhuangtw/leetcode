2dc0f9fa40134db1a2d0cd1ff5708a27cc1388a5
Tian-Liang Huang <tlhuang.tw@gmail.com>
2026-05-27 14:25:04 +0800

    Created 50.cpp

---
+++ 50.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+
+        if (n < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = 1.0;
+        while (N > 0) {
+            if (N % 2 != 0) {
+                ans *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+
+        return ans;
+    }
+};
+
