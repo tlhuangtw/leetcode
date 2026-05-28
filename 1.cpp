afeffa54a7c83238ab88930bd5cbb3687fb17601
Tian-Liang Huang <tlhuang.tw@gmail.com>
2026-05-28 11:04:01 +0800

    Created 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,20 +1,20 @@
-// Ref: https://leetcode.com/problems/two-sum/discuss/13/Accepted-C%2B%2B-O(n)-Solution
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n;
-        unordered_map<int, int> d;
-        vector<int> ans;
-        for (int i = 0; i < nums.size(); i++) {
-            n = target - nums[i];
-            if (d.find(n) != d.end()) {
-                ans.push_back(d[n]);
-                ans.push_back(i);
-                return ans;
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i) {
+            int complement = target - nums[i];
+
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
             }
-            d[nums[i]] = i;
+
+            numMap[nums[i]] = i;
         }
-        return ans;
+
+        return {};
     }
 };
+
