db77936d535d4eb11575a37497286f07c64e6b90
Tian-Liang Huang <tlhuang.tw@gmail.com>
2026-06-02 12:05:00 +0800

    Created 150.cpp

---
+++ 150.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+
+        for (const auto& token : tokens) {
+            if (token == "+" || token == "-" || token == "*" || token == "/") {
+                int b = s.top(); s.pop();
+                int a = s.top(); s.pop();
+
+                if (token == "+") {
+                    s.push(a + b);
+                } else if (token == "-") {
+                    s.push(a - b);
+                } else if (token == "*") {
+                    s.push(a * b);
+                } else {
+                    s.push(a / b);
+                }
+            } else {
+                s.push(stoi(token));
+            }
+        }
+
+        return s.top();
+    }
+};
+
