e1a554a579bbd9c02cc4ad70069aa2755ab8746a
Tian-Liang Huang <tlhuang.tw@gmail.com>
2026-06-06 17:12:48 +0800

    Created 703.cpp

---
+++ 703.cpp
@@ -0,0 +1,27 @@
+class KthLargest {
+private:
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    int k;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for (const auto& num : nums) {
+            add(num);
+        }
+    }
+    
+    int add(int val) {
+        minHeap.push(val);
+        if (minHeap.size() > k) {
+            minHeap.pop();
+        }
+        return minHeap.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+
