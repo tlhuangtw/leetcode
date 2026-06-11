d369c22364158b100edace2cbc0b6757c9c09a23
Tian-Liang Huang <tlhuang.tw@gmail.com>
2026-06-11 11:53:53 +0800

    Created 207.cpp

---
+++ 207.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> inDeg(numCourses);
+
+        for (const auto& e : prerequisites) {
+            adj[e[1]].push_back(e[0]);
+            inDeg[e[0]]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; ++i) {
+            if (inDeg[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        int count = 0;
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+            count++;
+
+            for (int neighbor : adj[cur]) {
+                if (--inDeg[neighbor] == 0) {
+                    q.push(neighbor); 
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+};
+
